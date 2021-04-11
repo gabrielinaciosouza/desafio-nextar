ecbd10f380ea01392c6bc27d655b755deb781858
gabrielinaciosouza <gabriel.sert9@gmail.com>
2021-04-11 10:56:31 -0300

    chance status bar to dark mode

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -1,11 +1,13 @@
 import 'package:desafio_nextar/ui/components/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../pages/pages.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
     return MaterialApp(
       title: 'Desafio Nextar',
       theme: makeAppTheme(),
