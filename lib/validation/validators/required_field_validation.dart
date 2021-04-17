9740ec399c091d6fd457834d3507dca44b085722
gabrielinaciosouza <gabriel.sert9@gmail.com>
2021-04-17 10:09:27 -0300

    ensure EmailValidation returns ValidationError.none if email is empty

---
+++ lib/validation/validators/required_field_validation.dart
@@ -0,0 +1,13 @@
+import 'package:desafio_nextar/presentation/protocols/protocols.dart';
+
+class RequiredFieldValidation implements FieldValidation {
+  final String field;
+
+  RequiredFieldValidation(this.field);
+
+  ValidationError? validate(String? value) {
+    return value?.isNotEmpty == true
+        ? ValidationError.none
+        : ValidationError.requiredField;
+  }
+}
---
+++ test/validation/validators/email_validation_test.dart
@@ -0,0 +1,22 @@
+import 'package:desafio_nextar/presentation/protocols/protocols.dart';
+import 'package:test/test.dart';
+
+class EmailValidation implements FieldValidation {
+  final String field;
+  EmailValidation(
+    this.field,
+  );
+
+  ValidationError? validate(String? value) {
+    return ValidationError.none;
+  }
+}
+
+void main() {
+  test('Should return ValidationError.none if email is empty', () {
+    final sut = EmailValidation('any_field');
+
+    final error = sut.validate('');
+    expect(error, ValidationError.none);
+  });
+}
