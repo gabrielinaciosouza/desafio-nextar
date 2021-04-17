f00be557b3e096041dda7a9c1de640f075a0f51a
gabrielinaciosouza <gabriel.sert9@gmail.com>
2021-04-17 18:16:45 -0300

    add mixin to splash page

--- lib/ui/pages/splash/splash_page.dart
+++ lib/ui/pages/splash/splash_page.dart
@@ -1,25 +1,23 @@
+import 'package:desafio_nextar/ui/mixins/mixins.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 import 'splash_presenter.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatelessWidget with NavigationManager {
   final SplashPresenter presenter;
 
   SplashPage({required this.presenter});
 
   @override
   Widget build(BuildContext context) {
+    presenter.checkAccount();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Splash'),
       ),
       body: Builder(builder: (context) {
-        presenter.navigateToStream.listen((page) {
-          if (page.isNotEmpty) {
-            Get.offAllNamed(page);
-          }
-        });
+        handleNavigation(presenter.navigateToStream, clear: true);
         return Center(
           child: CircularProgressIndicator(),
         );
