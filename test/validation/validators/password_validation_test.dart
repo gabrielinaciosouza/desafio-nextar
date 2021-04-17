4abb049297e121b766275ca4101ec4e0fc3603a0
gabrielinaciosouza <gabriel.sert9@gmail.com>
2021-04-17 11:22:38 -0300

    ensure password validation returns none if password is empty

---
+++ test/validation/validators/password_validation_test.dart
@@ -0,0 +1,38 @@
+import 'package:desafio_nextar/presentation/protocols/protocols.dart';
+import 'package:desafio_nextar/validation/validators/validators.dart';
+import 'package:test/test.dart';
+
+class PasswordValidation implements FieldValidation {
+  final String field;
+  PasswordValidation(
+    this.field,
+  );
+
+  ValidationError? validate(String? value) {
+    if (value!.isEmpty) {
+      return ValidationError.requiredField;
+    }
+
+    final isValid = value.length >= 6;
+    return isValid ? ValidationError.none : ValidationError.invalidField;
+  }
+}
+
+void main() {
+  late PasswordValidation sut;
+
+  setUp(() {
+    sut = PasswordValidation('any_field');
+  });
+  test('Should return ValidationError.none if password is empty', () {
+    expect(sut.validate(''), ValidationError.requiredField);
+  });
+
+  test('Should return ValidationError.none if password is valid', () {
+    expect(sut.validate('123456'), ValidationError.none);
+  });
+
+  test('Should return error if password is invalid', () {
+    expect(sut.validate('12345'), ValidationError.invalidField);
+  });
+}
