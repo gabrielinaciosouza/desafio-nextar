b74200afef6c1a1efa2dbe91a89bba3daee59f67
gabrielinaciosouza <gabriel.sert9@gmail.com>
2021-04-17 18:27:19 -0300

    add mixin to getx splash presenter

--- lib/presentation/presenters/getx_splash_presenter.dart
+++ lib/presentation/presenters/getx_splash_presenter.dart
@@ -1,25 +1,23 @@
 import 'package:get/get.dart';
 
 import '../../domain/usecases/usecases.dart';
-
+import '../mixins/mixins.dart';
 import '../../ui/pages/splash/splash.dart';
 
-class GetxSplashPresenter implements SplashPresenter {
+class GetxSplashPresenter extends GetxController
+    with NavigationManager
+    implements SplashPresenter {
   final LoadCurrentAccount loadCurrentAccount;
 
   GetxSplashPresenter({required this.loadCurrentAccount});
-  var _navigateTo = RxString('');
-
-  @override
-  Stream<String?>? get navigateToStream => _navigateTo.stream;
 
   @override
   Future<void> checkAccount() async {
     try {
       final account = await loadCurrentAccount.load();
-      _navigateTo.value = account.token.isEmpty ? '/login' : '/home';
+      navigateTo = account.token.isEmpty ? '/login' : '/home';
     } catch (error) {
-      _navigateTo.value = '/login';
+      navigateTo = '/login';
     }
   }
 }
