f2917bb76cf8ec492ede785d2bfd20af6a5f18b3
gabrielinaciosouza <gabriel.sert9@gmail.com>
2021-04-22 08:07:21 -0300

    make LocalLoadProducts implemets LoadProducts

--- lib/data/usecases/local_load_products/local_load_products.dart
+++ lib/data/usecases/local_load_products/local_load_products.dart
@@ -1,13 +1,15 @@
 import '../../../domain/helpers/helpers.dart';
 import '../../../domain/entities/entities.dart';
 
+import '../../../domain/usecases/usecases.dart';
 import '../../models/models.dart';
 import '../../cache/cache.dart';
 
-class LocalLoadProducts {
+class LocalLoadProducts implements LoadProducts {
   final FetchCacheStorage fetchCacheStorage;
 
   LocalLoadProducts({required this.fetchCacheStorage});
+  @override
   Future<List<ProductEntity>> load() async {
     try {
       final data = await fetchCacheStorage.fetch('products');
