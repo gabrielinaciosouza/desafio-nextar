668464788ea2a0cc334dfeeb3e4f5b7eda8be332
gabrielinaciosouza <gabriel.sert9@gmail.com>
2021-04-22 08:47:24 -0300

    move composite to main layer

--- lib/validation/validators/validators.dart
+++ lib/validation/validators/validators.dart
@@ -1,4 +1,4 @@
 export 'required_field_validation.dart';
 export 'email_validation.dart';
-export './validation_composite.dart';
+export '../../main/composites/validation_composite.dart';
 export './password_validation.dart';
