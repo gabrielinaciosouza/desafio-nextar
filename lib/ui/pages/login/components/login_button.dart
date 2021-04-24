b68379e1e517612f145844a4b2a557c246936c1a
gabrielinaciosouza <gabriel.sert9@gmail.com>
2021-04-24 16:24:20 -0300

    ensure ProductPage desable button if form is invalid

--- lib/ui/pages/login/components/login_button.dart
+++ lib/ui/pages/login/components/login_button.dart
@@ -15,33 +15,33 @@ class LoginButton extends StatelessWidget {
     final presenter = Provider.of<LoginPresenter>(context);
 
     return StreamBuilder<bool?>(
-        stream: presenter.isFormValidStream,
-        builder: (context, snapshot) {
-          return SizedBox(
-            height: MediaQuery.of(context).size.height > 800 ? 60 : 50,
-            width: double.infinity,
-            child: runningPlatform.currentPlatform == CurrentPlatform.isIOS
-                ? CupertinoButton(
-                    disabledColor:
-                        Theme.of(context).primaryColor.withAlpha(150),
-                    color: Theme.of(context).primaryColor,
-                    child: ResponsiveHeadline6(
-                      color: Theme.of(context).primaryColorLight,
-                      text: R.strings.enter,
-                    ),
-                    onPressed: snapshot.data == true
-                        ? () async => presenter.auth()
-                        : null)
-                : ElevatedButton(
-                    onPressed: snapshot.data == true
-                        ? () async => presenter.auth()
-                        : null,
-                    child: ResponsiveHeadline6(
-                      color: Theme.of(context).primaryColorLight,
-                      text: R.strings.enter,
-                    ),
+      stream: presenter.isFormValidStream,
+      builder: (context, snapshot) {
+        return SizedBox(
+          height: MediaQuery.of(context).size.height > 800 ? 60 : 50,
+          width: double.infinity,
+          child: runningPlatform.currentPlatform == CurrentPlatform.isIOS
+              ? CupertinoButton(
+                  disabledColor: Theme.of(context).primaryColor.withAlpha(150),
+                  color: Theme.of(context).primaryColor,
+                  child: ResponsiveHeadline6(
+                    color: Theme.of(context).primaryColorLight,
+                    text: R.strings.enter,
                   ),
-          );
-        });
+                  onPressed: snapshot.data == true
+                      ? () async => presenter.auth()
+                      : null)
+              : ElevatedButton(
+                  onPressed: snapshot.data == true
+                      ? () async => presenter.auth()
+                      : null,
+                  child: ResponsiveHeadline6(
+                    color: Theme.of(context).primaryColorLight,
+                    text: R.strings.enter,
+                  ),
+                ),
+        );
+      },
+    );
   }
 }
